a38fff7cba7a2f6bc47cf9dc7d3326f7bcb0008e
JayTu <jay_tu@fic.com.tw>
2019-10-31 15:38:22 +0800

    Code Style
    - adds .clang-format
    - perform clang-format
    
    Signed-off-by: JayTu <jay_tu@fic.com.tw>

--- J1939/J1939DataBase.cpp
+++ J1939/J1939DataBase.cpp
@@ -7,9 +7,9 @@
 
 #include "J1939DataBase.h"
 
-#include <string>
 #include <fstream>
 #include <streambuf>
+#include <string>
 
 #include <Diagnosis/Frames/DM1.h>
 
@@ -20,144 +20,128 @@
 
 #include <json/json.h>
 
-#define PGN_KEY						"pgn"
-#define SPNS_KEY					"spns"
-#define NAME_KEY					"name"
-#define NUMBER_KEY					"number"
-#define TYPE_KEY					"type"
-#define OFFSET_KEY					"offset"
-#define LENGTH_KEY					"length"
+#define PGN_KEY "pgn"
+#define SPNS_KEY "spns"
+#define NAME_KEY "name"
+#define NUMBER_KEY "number"
+#define TYPE_KEY "type"
+#define OFFSET_KEY "offset"
+#define LENGTH_KEY "length"
 
+#define NUM_FORMAT_GAIN_KEY "formatGain"
+#define NUM_FORMAT_OFFSET_KEY "formatOffset"
+#define NUM_BYTE_SIZE_KEY "byteSize"
+#define NUM_UNITS_KEY "units"
 
-#define NUM_FORMAT_GAIN_KEY			"formatGain"
-#define NUM_FORMAT_OFFSET_KEY		"formatOffset"
-#define NUM_BYTE_SIZE_KEY			"byteSize"
-#define NUM_UNITS_KEY				"units"
+#define STAT_BIT_OFFSET_KEY "bitOffset"
+#define STAT_BIT_SIZE_KEY "bitSize"
 
-#define STAT_BIT_OFFSET_KEY			"bitOffset"
-#define STAT_BIT_SIZE_KEY			"bitSize"
+#define STAT_DESCRIPTIONS_KEY "descriptions"
 
-#define STAT_DESCRIPTIONS_KEY       "descriptions"
+namespace J1939
+{
+J1939DataBase::J1939DataBase() : mErrorCode(ERROR_OK) {}
 
+J1939DataBase::~J1939DataBase() {}
 
-
-
-
-namespace J1939 {
-
-J1939DataBase::J1939DataBase() : mErrorCode(ERROR_OK) {
-
-}
-
-J1939DataBase::~J1939DataBase() {
-}
-
-
-bool J1939DataBase::parseJsonFile(const std::string& file) {
-
+bool J1939DataBase::parseJsonFile(const std::string &file)
+{
 	Json::Value data;
 	std::ifstream fileStream(file.c_str());
 
-
-	if(!fileStream.is_open()) {
+	if (!fileStream.is_open()) {
 		mErrorCode = ERROR_FILE_NOT_FOUND;
 		return false;
 	}
 
-
 	Json::CharReaderBuilder rbuilder;
 
 	std::string errs;
-	if(!Json::parseFromStream(rbuilder, fileStream, &data, &errs)) {
+	if (!Json::parseFromStream(rbuilder, fileStream, &data, &errs)) {
 		mErrorCode = ERROR_JSON_SYNTAX;
 		return false;
 	}
 
-
-	//If we return in the middle of the method, there was a problem with some of the tokens
+	// If we return in the middle of the method, there was a problem with some
+	// of the tokens
 	mErrorCode = ERROR_UNEXPECTED_TOKENS;
 
-	if(!data.isArray()) {
+	if (!data.isArray()) {
 		return false;
 	}
 
-	for(unsigned int i = 0; i < data.size(); ++i) {
+	for (unsigned int i = 0; i < data.size(); ++i) {
+		const Json::Value &jsonFrame = data[i];
 
-		const Json::Value& jsonFrame = data[i];
-
-        if(!jsonFrame.isMember(PGN_KEY) || !jsonFrame[PGN_KEY].isUInt()) {
+		if (!jsonFrame.isMember(PGN_KEY) || !jsonFrame[PGN_KEY].isUInt()) {
 			return false;
 		}
 
-        u32 pgn = jsonFrame[PGN_KEY].asUInt();
+		u32 pgn = jsonFrame[PGN_KEY].asUInt();
 
-        switch(pgn) {
-        case DM1_PGN:		//Not permitted this token. Dangerous for the good performance of the software.
-        	mErrorCode = ERROR_FORBIDDEN_TOKEN;
-        	return false;
-        default:
-        	break;
-
-        }
+		switch (pgn) {
+		case DM1_PGN: // Not permitted this token. Dangerous for the good
+					  // performance of the software.
+			mErrorCode = ERROR_FORBIDDEN_TOKEN;
+			return false;
+		default:
+			break;
+		}
 
 		GenericFrame frame(pgn);
 
-        if(jsonFrame.isMember(NAME_KEY) && jsonFrame[NAME_KEY].isString()) {      //Optional
-            frame.setName(jsonFrame[NAME_KEY].asString());
-        }
+		if (jsonFrame.isMember(NAME_KEY) &&
+			jsonFrame[NAME_KEY].isString()) { // Optional
+			frame.setName(jsonFrame[NAME_KEY].asString());
+		}
 
-        if(jsonFrame.isMember(LENGTH_KEY) && jsonFrame[LENGTH_KEY].isUInt()) {		//Optional
-        	frame.setLength(jsonFrame[LENGTH_KEY].asUInt());
-        }
+		if (jsonFrame.isMember(LENGTH_KEY) &&
+			jsonFrame[LENGTH_KEY].isUInt()) { // Optional
+			frame.setLength(jsonFrame[LENGTH_KEY].asUInt());
+		}
 
+		if (jsonFrame.isMember(SPNS_KEY)) {
+			const Json::Value &spns = jsonFrame[SPNS_KEY];
 
-
-		if(jsonFrame.isMember(SPNS_KEY)) {
-			const Json::Value& spns = jsonFrame[SPNS_KEY];
-
-			if(!spns.isArray()) {
+			if (!spns.isArray()) {
 				return false;
 			}
 
-			for(unsigned int j = 0; j < spns.size(); ++j) {
-				const Json::Value& spn = spns[j];
+			for (unsigned int j = 0; j < spns.size(); ++j) {
+				const Json::Value &spn = spns[j];
 
-				if(!spn.isMember(NAME_KEY) || !spn.isMember(NUMBER_KEY) || !spn.isMember(TYPE_KEY)) {
+				if (!spn.isMember(NAME_KEY) || !spn.isMember(NUMBER_KEY) ||
+					!spn.isMember(TYPE_KEY)) {
 					return false;
-
 				}
 
-				const Json::Value& name = spn[NAME_KEY];
-				const Json::Value& number = spn[NUMBER_KEY];
-				const Json::Value& type = spn[TYPE_KEY];
+				const Json::Value &name = spn[NAME_KEY];
+				const Json::Value &number = spn[NUMBER_KEY];
+				const Json::Value &type = spn[TYPE_KEY];
 				const Json::Value offset = spn[OFFSET_KEY];
 
-
-				if(!name.isString() || !number.isUInt() || !type.isUInt()) {
+				if (!name.isString() || !number.isUInt() || !type.isUInt()) {
 					return false;
 				}
 
-
-				switch(type.asUInt()) {
-
+				switch (type.asUInt()) {
 				case SPN::SPN_NUMERIC:
 
-					if(!parseSPNNumeric(frame, spn)) {
+					if (!parseSPNNumeric(frame, spn)) {
 						return false;
 					}
 					break;
 
-
 				case SPN::SPN_STATUS:
 
-					if(!parseSPNStatus(frame, spn)) {
+					if (!parseSPNStatus(frame, spn)) {
 						return false;
 					}
 					break;
 
 				case SPN::SPN_STRING:
 
-					if(!parseSPNString(frame, spn)) {
+					if (!parseSPNString(frame, spn)) {
 						return false;
 					}
 
@@ -168,182 +152,167 @@ bool J1939DataBase::parseJsonFile(const std::string& file) {
 					return false;
 					break;
 				}
-
-
-            }
-
+			}
 		}
-        mFrames.push_back(frame);
-
-
+		mFrames.push_back(frame);
 	}
 
 	mErrorCode = ERROR_OK;
 
 	return true;
-
 }
 
-
-bool J1939DataBase::writeJsonFile(const std::string& file) {
-
+bool J1939DataBase::writeJsonFile(const std::string &file)
+{
 	Json::Value data;
 
-    std::ofstream ofs (file.c_str(), std::ofstream::out | std::ofstream::trunc);
+	std::ofstream ofs(file.c_str(), std::ofstream::out | std::ofstream::trunc);
 
-	if(!ofs.is_open()) {
+	if (!ofs.is_open()) {
 		return false;
 	}
 
 	unsigned int pos;
 
-	for(std::vector<GenericFrame>::const_iterator frame = mFrames.begin(); frame != mFrames.end(); ++frame) {
-
-
+	for (std::vector<GenericFrame>::const_iterator frame = mFrames.begin();
+		 frame != mFrames.end(); ++frame) {
 		pos = frame - mFrames.begin();
-		Json::Value& jsonFrame = data[pos];
+		Json::Value &jsonFrame = data[pos];
 
 		jsonFrame[PGN_KEY] = frame->getPGN();
-        jsonFrame[NAME_KEY] = frame->getName();
-        jsonFrame[LENGTH_KEY] = frame->getDataLength();
-
-
+		jsonFrame[NAME_KEY] = frame->getName();
+		jsonFrame[LENGTH_KEY] = frame->getDataLength();
 
 		std::set<u32> spns = frame->getSPNNumbers();
 
+		if (!spns.empty()) {
+			Json::Value &jsonSPNs = jsonFrame[SPNS_KEY];
+			int i = 0;
+			for (std::set<u32>::const_iterator iter = spns.begin();
+				 iter != spns.end(); ++iter) {
+				Json::Value &jsonSPN = jsonSPNs[i++];
 
-        if(!spns.empty()) {
-            Json::Value& jsonSPNs = jsonFrame[SPNS_KEY];
-            int i = 0;
-            for(std::set<u32>::const_iterator iter = spns.begin(); iter != spns.end(); ++iter) {
+				const SPN *spn = frame->getSPN(*iter);
 
-                Json::Value& jsonSPN = jsonSPNs[i++];
+				jsonSPN[TYPE_KEY] = spn->getType();
+				jsonSPN[NUMBER_KEY] = spn->getSpnNumber();
+				jsonSPN[NAME_KEY] = spn->getName();
+				jsonSPN[OFFSET_KEY] = spn->getOffset();
 
-                const SPN* spn = frame->getSPN(*iter);
+				switch (spn->getType()) {
+				case SPN::SPN_NUMERIC:
+					writeSPNNumeric(spn, jsonSPN);
+					break;
 
-
-                jsonSPN[TYPE_KEY] = 		spn->getType();
-                jsonSPN[NUMBER_KEY] = 		spn->getSpnNumber();
-                jsonSPN[NAME_KEY] = 		spn->getName();
-                jsonSPN[OFFSET_KEY] = 		spn->getOffset();
-
-                switch(spn->getType()) {
-                case SPN::SPN_NUMERIC:
-                    writeSPNNumeric(spn, jsonSPN);
-                    break;
-
-                case SPN::SPN_STATUS:
-                    writeSPNStatus(spn, jsonSPN);
-                    break;
-                default:
-                    break;
-                }
-
-            }
-        }
-
+				case SPN::SPN_STATUS:
+					writeSPNStatus(spn, jsonSPN);
+					break;
+				default:
+					break;
+				}
+			}
+		}
 	}
 
-    ofs << data;
-
+	ofs << data;
 
 	return true;
-
 }
 
-
-const std::vector<GenericFrame>& J1939DataBase::getParsedFrames() const {
+const std::vector<GenericFrame> &J1939DataBase::getParsedFrames() const
+{
 	return mFrames;
 }
 
-
-bool J1939DataBase::parseSPNNumeric(GenericFrame& frame, const Json::Value& spn) {
-
-	if(!spn.isMember(NUM_FORMAT_GAIN_KEY)
-			|| !spn.isMember(NUM_FORMAT_OFFSET_KEY) || !spn.isMember(NUM_BYTE_SIZE_KEY) || !spn.isMember(NUM_UNITS_KEY) || !spn.isMember(OFFSET_KEY)) {
+bool J1939DataBase::parseSPNNumeric(GenericFrame &frame, const Json::Value &spn)
+{
+	if (!spn.isMember(NUM_FORMAT_GAIN_KEY) ||
+		!spn.isMember(NUM_FORMAT_OFFSET_KEY) ||
+		!spn.isMember(NUM_BYTE_SIZE_KEY) || !spn.isMember(NUM_UNITS_KEY) ||
+		!spn.isMember(OFFSET_KEY)) {
 		return false;
 	}
 
-	const Json::Value& name = 				spn[NAME_KEY];
-	const Json::Value& number =	 			spn[NUMBER_KEY];
-	const Json::Value& offset = 			spn[OFFSET_KEY];
-	const Json::Value& formatGain = 		spn[NUM_FORMAT_GAIN_KEY];
-	const Json::Value& formatOffset = 		spn[NUM_FORMAT_OFFSET_KEY];
-	const Json::Value& byteSize = 			spn[NUM_BYTE_SIZE_KEY];
-	const Json::Value& units = 				spn[NUM_UNITS_KEY];
+	const Json::Value &name = spn[NAME_KEY];
+	const Json::Value &number = spn[NUMBER_KEY];
+	const Json::Value &offset = spn[OFFSET_KEY];
+	const Json::Value &formatGain = spn[NUM_FORMAT_GAIN_KEY];
+	const Json::Value &formatOffset = spn[NUM_FORMAT_OFFSET_KEY];
+	const Json::Value &byteSize = spn[NUM_BYTE_SIZE_KEY];
+	const Json::Value &units = spn[NUM_UNITS_KEY];
 
-	if(!formatGain.isDouble() || !formatOffset.isDouble() || !byteSize.isUInt() || !units.isString() || !offset.isUInt()) {
+	if (!formatGain.isDouble() || !formatOffset.isDouble() ||
+		!byteSize.isUInt() || !units.isString() || !offset.isUInt()) {
 		return false;
 	}
 
-	//Check if bytesize does not exceed the permitted values
-	if(byteSize.asUInt() > SPN_NUMERIC_MAX_BYTE_SYZE || byteSize.asUInt() == 0) {
+	// Check if bytesize does not exceed the permitted values
+	if (byteSize.asUInt() > SPN_NUMERIC_MAX_BYTE_SYZE ||
+		byteSize.asUInt() == 0) {
 		mErrorCode = ERROR_OUT_OF_RANGE;
 		return false;
 	}
 
-
-    SPNNumeric spnNum(number.asUInt(), name.asString(), offset.asUInt(), formatGain.asDouble(), formatOffset.asDouble(), byteSize.asUInt(), units.asString());
+	SPNNumeric spnNum(number.asUInt(), name.asString(), offset.asUInt(),
+					  formatGain.asDouble(), formatOffset.asDouble(),
+					  byteSize.asUInt(), units.asString());
 
 	frame.registerSPN(spnNum);
 
 	return true;
-
 }
 
-
-bool J1939DataBase::parseSPNStatus(GenericFrame& frame, const Json::Value& spn) {
-
-	if(!spn.isMember(STAT_BIT_OFFSET_KEY) || !spn.isMember(STAT_BIT_SIZE_KEY) || !spn.isMember(OFFSET_KEY)) {
+bool J1939DataBase::parseSPNStatus(GenericFrame &frame, const Json::Value &spn)
+{
+	if (!spn.isMember(STAT_BIT_OFFSET_KEY) ||
+		!spn.isMember(STAT_BIT_SIZE_KEY) || !spn.isMember(OFFSET_KEY)) {
 		return false;
 	}
 
-	const Json::Value& name = spn[NAME_KEY];
-	const Json::Value& number = spn[NUMBER_KEY];
-	const Json::Value& offset = spn[OFFSET_KEY];
-	const Json::Value& bitOffset = spn[STAT_BIT_OFFSET_KEY];
-	const Json::Value& bitSize = spn[STAT_BIT_SIZE_KEY];
+	const Json::Value &name = spn[NAME_KEY];
+	const Json::Value &number = spn[NUMBER_KEY];
+	const Json::Value &offset = spn[OFFSET_KEY];
+	const Json::Value &bitOffset = spn[STAT_BIT_OFFSET_KEY];
+	const Json::Value &bitSize = spn[STAT_BIT_SIZE_KEY];
 
-
-	if(!bitOffset.isUInt() || !bitSize.isUInt() || !offset.isUInt()) {
+	if (!bitOffset.isUInt() || !bitSize.isUInt() || !offset.isUInt()) {
 		return false;
 	}
 
-	//Check the limits of the input values
-	if(bitSize.asUInt() == 0 || (bitSize.asUInt() + bitOffset.asUInt()) > 8) {
+	// Check the limits of the input values
+	if (bitSize.asUInt() == 0 || (bitSize.asUInt() + bitOffset.asUInt()) > 8) {
 		mErrorCode = ERROR_OUT_OF_RANGE;
 		return false;
 	}
 
+	// Check for added descriptions
 
-    //Check for added descriptions
+	SPNStatusSpec::DescMap valueToDesc;
 
-    SPNStatusSpec::DescMap valueToDesc;
+	if (spn.isMember(STAT_DESCRIPTIONS_KEY)) {
+		const Json::Value &descriptions = spn[STAT_DESCRIPTIONS_KEY];
 
-    if(spn.isMember(STAT_DESCRIPTIONS_KEY)) {
-        const Json::Value& descriptions = spn[STAT_DESCRIPTIONS_KEY];
+		for (unsigned int i = 0; i < descriptions.size(); ++i) {
+			if (descriptions[i].isNull()) {
+				continue;
+			}
 
-        for(unsigned int i = 0; i < descriptions.size(); ++i) {
-            if(descriptions[i].isNull()){
-                continue;
-            }
+			valueToDesc[i] = descriptions[i].asString();
+		}
+	}
 
-            valueToDesc[i] = descriptions[i].asString();
-        }
-    }
-
-    SPNStatus spnStat(number.asUInt(), name.asString(), offset.asUInt(), bitOffset.asUInt(), bitSize.asUInt(), valueToDesc);
+	SPNStatus spnStat(number.asUInt(), name.asString(), offset.asUInt(),
+					  bitOffset.asUInt(), bitSize.asUInt(), valueToDesc);
 
 	frame.registerSPN(spnStat);
 
 	return true;
-
 }
 
-bool J1939DataBase::parseSPNString(GenericFrame& frame, const Json::Value& spn) {
-
-	const Json::Value& name = spn[NAME_KEY];
-	const Json::Value& number = spn[NUMBER_KEY];
+bool J1939DataBase::parseSPNString(GenericFrame &frame, const Json::Value &spn)
+{
+	const Json::Value &name = spn[NAME_KEY];
+	const Json::Value &number = spn[NUMBER_KEY];
 
 	SPNString spnStr(number.asUInt(), name.asString());
 
@@ -352,9 +321,9 @@ bool J1939DataBase::parseSPNString(GenericFrame& frame, const Json::Value& spn) 
 	return true;
 }
 
-void J1939DataBase::writeSPNNumeric(const SPN* spn, Json::Value& jsonSpn) {
-
-	const SPNNumeric* spnNum = (SPNNumeric*)spn;
+void J1939DataBase::writeSPNNumeric(const SPN *spn, Json::Value &jsonSpn)
+{
+	const SPNNumeric *spnNum = (SPNNumeric *)spn;
 
 	jsonSpn[NUM_BYTE_SIZE_KEY] = spnNum->getByteSize();
 	jsonSpn[NUM_FORMAT_GAIN_KEY] = spnNum->getFormatGain();
@@ -362,41 +331,37 @@ void J1939DataBase::writeSPNNumeric(const SPN* spn, Json::Value& jsonSpn) {
 	jsonSpn[NUM_UNITS_KEY] = spnNum->getUnits();
 }
 
-
-void J1939DataBase::writeSPNStatus(const SPN* spn, Json::Value& jsonSpn) {
-
-	const SPNStatus* spnStat = (SPNStatus*)spn;
+void J1939DataBase::writeSPNStatus(const SPN *spn, Json::Value &jsonSpn)
+{
+	const SPNStatus *spnStat = (SPNStatus *)spn;
 
 	jsonSpn[STAT_BIT_OFFSET_KEY] = spnStat->getBitOffset();
 	jsonSpn[STAT_BIT_SIZE_KEY] = spnStat->getBitSize();
 
-    //Check if there are descriptions defined for the different status numbers
-    SPNStatus::DescMap descriptions = spnStat->getValueDescriptionsMap();
+	// Check if there are descriptions defined for the different status numbers
+	SPNStatus::DescMap descriptions = spnStat->getValueDescriptionsMap();
 
-    if(descriptions.empty()) {
-        return;
-    }
+	if (descriptions.empty()) {
+		return;
+	}
 
-    Json::Value& description = jsonSpn[STAT_DESCRIPTIONS_KEY];
+	Json::Value &description = jsonSpn[STAT_DESCRIPTIONS_KEY];
 
-    for(auto iter = descriptions.begin(); iter != descriptions.end(); ++iter) {
-        description[iter->first] = iter->second;
-    }
-
+	for (auto iter = descriptions.begin(); iter != descriptions.end(); ++iter) {
+		description[iter->first] = iter->second;
+	}
 }
 
-void J1939DataBase::addFrame(const GenericFrame& frame) {
-
+void J1939DataBase::addFrame(const GenericFrame &frame)
+{
 	mFrames.push_back(frame);
-
 }
 
-void J1939DataBase::clear() {
-
+void J1939DataBase::clear()
+{
 	mFrames.clear();
 	mErrorCode = ERROR_OK;
-
 }
 
 } /* namespace J1939 */
-//Json::CharReaderBuilder rbuilder;
+// Json::CharReaderBuilder rbuilder;
--- J1939/J1939Factory.cpp
+++ J1939/J1939Factory.cpp
@@ -5,117 +5,105 @@
  *      Author: famez
  */
 
+#include <J1939DataBase.h>
 #include <J1939Factory.h>
 #include <J1939Frame.h>
-#include <J1939DataBase.h>
 
+#include <Addressing/AddressClaimFrame.h>
+#include <Diagnosis/Frames/DM1.h>
+#include <FMS/TellTale/FMS1Frame.h>
+#include <Frames/RequestFrame.h>
 #include <Transport/TPCMFrame.h>
 #include <Transport/TPDTFrame.h>
-#include <Diagnosis/Frames/DM1.h>
-#include <Addressing/AddressClaimFrame.h>
-#include <Frames/RequestFrame.h>
-#include <FMS/TellTale/FMS1Frame.h>
-#include <Diagnosis/Frames/DM1.h>
 
-
-
-
-namespace J1939 {
-
-
-J1939Factory::J1939Factory() {
+namespace J1939
+{
+J1939Factory::J1939Factory()
+{
 	registerPredefinedFrames();
 }
 
-
-J1939Factory::~J1939Factory() {
-
-    unregisterAllFrames();
-
+J1939Factory::~J1939Factory()
+{
+	unregisterAllFrames();
 }
 
-void J1939Factory::unregisterAllFrames() {
-    for(std::map<u32, J1939Frame*>::const_iterator iter = mFrames.begin(); iter != mFrames.end(); iter++) {
-        if(iter->second)
-            delete iter->second;
-    }
-    mFrames.clear();
+void J1939Factory::unregisterAllFrames()
+{
+	for (std::map<u32, J1939Frame *>::const_iterator iter = mFrames.begin();
+		 iter != mFrames.end(); iter++) {
+		if (iter->second)
+			delete iter->second;
+	}
+	mFrames.clear();
 }
 
-std::unique_ptr<J1939Frame> J1939Factory::getJ1939Frame(u32 id, const u8* data, size_t length) {
-
-	J1939Frame* frame = NULL, *retFrame = NULL;
+std::unique_ptr<J1939Frame> J1939Factory::getJ1939Frame(u32 id, const u8 *data,
+							size_t length)
+{
+	J1939Frame *frame = NULL, *retFrame = NULL;
 
 	u32 pgn = ((id >> J1939_PGN_OFFSET) & J1939_PGN_MASK);
 
-	//Check if PDU format belongs to the first group
-	if(((pgn >> J1939_PDU_FMT_OFFSET) & J1939_PDU_FMT_MASK) < PDU_FMT_DELIMITER) {
+	// Check if PDU format belongs to the first group
+	if (((pgn >> J1939_PDU_FMT_OFFSET) & J1939_PDU_FMT_MASK) <
+		PDU_FMT_DELIMITER) {
 		pgn &= (J1939_PDU_FMT_MASK << J1939_PDU_FMT_OFFSET);
 	}
 
-	std::map<u32, J1939Frame*>::iterator iter = mFrames.find(pgn);
+	std::map<u32, J1939Frame *>::iterator iter = mFrames.find(pgn);
 
-	if(iter == mFrames.end() || ((frame = iter->second) == NULL)) {
-        return std::unique_ptr<J1939Frame>(nullptr);
+	if (iter == mFrames.end() || ((frame = iter->second) == NULL)) {
+		return std::unique_ptr<J1939Frame>(nullptr);
 	}
 
 	retFrame = frame->clone();
 
-	if(retFrame) {
+	if (retFrame) {
 		retFrame->decode(id, data, length);
 	}
-    return std::unique_ptr<J1939Frame>(retFrame);
-
+	return std::unique_ptr<J1939Frame>(retFrame);
 }
 
+std::unique_ptr<J1939Frame> J1939Factory::getJ1939Frame(u32 pgn)
+{
+	J1939Frame *frame = nullptr, *retFrame = nullptr;
 
-std::unique_ptr<J1939Frame> J1939Factory::getJ1939Frame(u32 pgn) {
+	std::map<u32, J1939Frame *>::iterator iter = mFrames.find(pgn);
 
-
-	J1939Frame* frame = nullptr, *retFrame = nullptr;
-
-	std::map<u32, J1939Frame*>::iterator iter = mFrames.find(pgn);
-
-	if(iter == mFrames.end() || ((frame = iter->second) == NULL)) {
-		//printf("Pgn: %u not found", pgn);
+	if (iter == mFrames.end() || ((frame = iter->second) == NULL)) {
+		// printf("Pgn: %u not found", pgn);
 		return std::unique_ptr<J1939Frame>(nullptr);
 	}
 
 	retFrame = frame->clone();
 
 	return std::unique_ptr<J1939Frame>(retFrame);
-
 }
 
-std::unique_ptr<J1939Frame> J1939Factory::getJ1939Frame(const std::string& name) {
-
-
-	for(auto iter = mFrames.begin(); iter != mFrames.end(); ++iter) {
-		if(iter->second != nullptr && iter->second->getName() == name) {
-
+std::unique_ptr<J1939Frame> J1939Factory::getJ1939Frame(const std::string &name)
+{
+	for (auto iter = mFrames.begin(); iter != mFrames.end(); ++iter) {
+		if (iter->second != nullptr && iter->second->getName() == name) {
 			return std::unique_ptr<J1939Frame>(iter->second->clone());
-
 		}
 	}
 
 	return std::unique_ptr<J1939Frame>(nullptr);
 }
 
-
-bool J1939Factory::registerFrame(const J1939Frame& frame) {
-
-	if(mFrames.find(frame.getPGN()) == mFrames.end()) {
+bool J1939Factory::registerFrame(const J1939Frame &frame)
+{
+	if (mFrames.find(frame.getPGN()) == mFrames.end()) {
 		mFrames[frame.getPGN()] = frame.clone();
-        return true;
-    } else {
-        return false;
-    }
+		return true;
+	} else {
+		return false;
+	}
 }
 
-
-
-void J1939Factory::registerPredefinedFrames() {
-
+void J1939Factory::registerPredefinedFrames()
+{
 	{
 		TPCMFrame frame;
 		registerFrame(frame);
@@ -126,11 +114,10 @@ void J1939Factory::registerPredefinedFrames() {
 		registerFrame(frame);
 	}
 
-    {
-    	FMS1Frame frame;
-    	registerFrame(frame);
-    }
-
+	{
+		FMS1Frame frame;
+		registerFrame(frame);
+	}
 
 	{
 		DM1 frame;
@@ -146,50 +133,43 @@ void J1939Factory::registerPredefinedFrames() {
 		RequestFrame frame;
 		registerFrame(frame);
 	}
-
 }
 
-std::set<u32> J1939Factory::getAllRegisteredPGNs() const {
-
+std::set<u32> J1939Factory::getAllRegisteredPGNs() const
+{
 	std::set<u32> pgns;
 
-
-	for(std::map<u32, J1939Frame*>::const_iterator iter = mFrames.begin(); iter != mFrames.end(); iter++) {
+	for (std::map<u32, J1939Frame *>::const_iterator iter = mFrames.begin();
+		 iter != mFrames.end(); iter++) {
 		pgns.insert(iter->first);
 	}
 
 	return pgns;
-
 }
 
-void J1939Factory::unRegisterFrame(u32 pgn) {
-
+void J1939Factory::unRegisterFrame(u32 pgn)
+{
 	auto iter = mFrames.find(pgn);
 
-	if(iter != mFrames.end()) {
-
+	if (iter != mFrames.end()) {
 		delete iter->second;
 		mFrames.erase(iter);
-
 	}
-
 }
 
-bool J1939Factory::registerDatabaseFrames(const std::string& file) {
-
-	//Read database if available
+bool J1939Factory::registerDatabaseFrames(const std::string &file)
+{
+	// Read database if available
 	J1939DataBase database;
 
-	if(!database.parseJsonFile(file)) {
+	if (!database.parseJsonFile(file))
 		return false;
-	}
 
-	const std::vector<GenericFrame>& ddbbFrames = database.getParsedFrames();
+	const std::vector<GenericFrame> &ddbbFrames = database.getParsedFrames();
 
-	//Register all the frames listed in the database
-	for(auto iter = ddbbFrames.begin(); iter != ddbbFrames.end(); ++iter) {
+	// Register all the frames listed in the database
+	for (auto iter = ddbbFrames.begin(); iter != ddbbFrames.end(); ++iter)
 		registerFrame(*iter);
-	}
 
 	return true;
 }

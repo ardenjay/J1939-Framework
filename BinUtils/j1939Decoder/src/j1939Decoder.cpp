0df41d28ee4ec87821a6ae54baae764da7e0d503
JayTu <jay_tu@fic.com.tw>
2019-11-27 17:28:15 +0800

    j1939Decoder: move BAM process to processBAM
    Also cleans some comments
    
    Signed-off-by: JayTu <jay_tu@fic.com.tw>

--- BinUtils/j1939Decoder/src/j1939Decoder.cpp
+++ BinUtils/j1939Decoder/src/j1939Decoder.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Name        : j1939Encoder.cpp
-// Author      :
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -38,15 +30,9 @@ bool silent = false;
 
 std::basic_string<u8> decodeData(const std::string &data)
 {
-	// At this point we have the options, we build regular expressions to
-	// validate them
-
-	// Data regex will match the format XXXXXXXXXXXXXXXX where X is an
-	// hexadecimal digit.
-	std::string dataRegex("^(\\s{0,1}([0-9a-fA-F][0-9a-fA-F]))+$");
-
-	regex_t regex;
 	int retVal;
+	regex_t regex;
+	std::string dataRegex("^(\\s{0,1}([0-9a-fA-F][0-9a-fA-F]))+$");
 
 	retVal = regcomp(&regex, dataRegex.c_str(), REG_EXTENDED);
 	if (retVal) {
@@ -62,14 +48,11 @@ std::basic_string<u8> decodeData(const std::string &data)
 		std::cerr << "Problem executing reg expression for ID" << std::endl;
 		exit(2);
 	}
-
 	regfree(&regex);
 
 	std::stringstream dataStream;
 	dataStream << std::hex << data;
 
-	// String where to store the data of the frame ready to be passed to the
-	// frame factory with the correct format.
 	std::basic_string<u8> formattedData;
 	std::string token;
 
@@ -77,22 +60,22 @@ std::basic_string<u8> decodeData(const std::string &data)
 		dataStream >> std::ws >> std::setw(2) >> token;
 		formattedData.push_back(
 			static_cast<u8>(std::stoul(token, nullptr, 16)));
-	} while (!(dataStream.rdstate() & std::ios_base::eofbit)); // End of file
+	} while (!(dataStream.rdstate() & std::ios_base::eofbit));
 
 	return formattedData;
 }
 
 u32 decodeID(const std::string &id)
 {
-	// ID regex will match the format XXXXXXXX where X is an hexadecimal digit.
-	// The id in extended format in CAN has a length of 29 bits. It must be at
-	// least 8 digits.
+	int retVal;
+	regex_t regex;
+
+	/* ID regex will match the format XXXXXXXX
+	 * where X is an hexadecimal digit.
+	 */
 	std::string idRegex(
 		"^[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]"
 		"[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]$");
-
-	regex_t regex;
-	int retVal;
 
 	retVal = regcomp(&regex, idRegex.c_str(), 0);
 	if (retVal) {
@@ -108,14 +91,54 @@ u32 decodeID(const std::string &id)
 		std::cerr << "Problem executing reg expression for ID" << std::endl;
 		exit(2);
 	}
-
 	regfree(&regex);
 
-	// Convert the introduced string to a number to be interpreted by the frame
-	// factory when using it.
 	u32 formattedId = std::stoul(id, nullptr, 16);
-
 	return formattedId;
+}
+
+int processBAM(BamReassembler& reassembler)
+{
+	string id, data;
+	u32 formattedId;
+	basic_string<u8> formattedData;
+	unique_ptr<J1939Frame> frame;
+
+	while (!reassembler.reassembledFramesPending()) {
+		std::cout << "Id: " << std::endl;
+		std::getline(std::cin, id);
+		formattedId = decodeID(id);
+
+		std::cout << "Data: " << std::endl;
+		std::getline(std::cin, data);
+		formattedData = decodeData(data);
+
+		try {
+			frame = J1939Factory::getInstance().getJ1939Frame(
+					formattedId, formattedData.c_str(), formattedData.size());
+
+			if (frame) {
+				reassembler.handleFrame(*frame);
+			} else {
+				std::cerr << "Frame not identified" << std::endl;
+				return -EINVAL;
+			}
+
+		} catch (J1939DecodeException &e) {
+			std::cerr << "Error decoding frame: " << e.getMessage()
+				<< std::endl;
+			return -EINVAL;
+		}
+	}
+
+	frame = reassembler.dequeueReassembledFrame();
+
+	// Check again if the frame is well decoded
+	if (!frame) {
+		std::cerr << "Frame not identified" << std::endl;
+		return -EINVAL;
+	}
+	return 0;
 }
 
 int process(string id, string data)
@@ -127,13 +150,6 @@ int process(string id, string data)
 		return -EINVAL;
 	}
 
-	// At this point we have the options, we build regular expressions to
-	// validate them
-
-	u32 formattedId = decodeID(id);
-
-	std::basic_string<u8> formattedData = decodeData(data);
-
 	if (silent == false)
 		std::cout << "Loaded Database: " << DATABASE_PATH << std::endl;
 
@@ -142,13 +158,14 @@ int process(string id, string data)
 		return -EIO;
 	}
 
-	// The rest is easy
+	/* get a J1939Frame */
 	std::unique_ptr<J1939Frame> frame;
+	u32 formattedId = decodeID(id);
+	std::basic_string<u8> formattedData = decodeData(data);
 
 	try {
 		frame = J1939Factory::getInstance().getJ1939Frame(
 			formattedId, formattedData.c_str(), formattedData.size());
-
 	} catch (J1939DecodeException &e) {
 		std::cerr << "Error decoding frame: " << e.getMessage() << std::endl;
 		return -EIO;
@@ -161,51 +178,14 @@ int process(string id, string data)
 		return -EINVAL;
 	}
 
-	// Check if it is part of the BAM protocol
-
 	if (reassembler.toBeHandled(*frame)) {
-		std::cout << "Bam frame detected... Waiting for the rest of frames to "
-					 "be received..."
-				  << std::endl;
+		cout << "Bam frame detected... Waiting for the rest of frames to "
+			"be received..." << endl;
 
 		// First frame should be CM frame, otherwise, it is likely that we
 		// remain in the loop forever
 		reassembler.handleFrame(*frame);
-
-		while (!reassembler.reassembledFramesPending()) {
-			std::cout << "Id: " << std::endl;
-			std::getline(std::cin, id);
-			formattedId = decodeID(id);
-
-			std::cout << "Data: " << std::endl;
-			std::getline(std::cin, data);
-			formattedData = decodeData(data);
-
-			try {
-				frame = J1939Factory::getInstance().getJ1939Frame(
-					formattedId, formattedData.c_str(), formattedData.size());
-
-				if (frame) {
-					reassembler.handleFrame(*frame);
-				} else {
-					std::cerr << "Frame not identified" << std::endl;
-					return -EINVAL;
-				}
-
-			} catch (J1939DecodeException &e) {
-				std::cerr << "Error decoding frame: " << e.getMessage()
-						  << std::endl;
-				return -EINVAL;
-			}
-		}
-
-		frame = reassembler.dequeueReassembledFrame();
-
-		// Check again if the frame is well decoded
-		if (!frame) {
-			std::cerr << "Frame not identified" << std::endl;
-			return -EINVAL;
-		}
+		processBAM(reassembler);
 	}
 
 	std::cout << frame->toString();

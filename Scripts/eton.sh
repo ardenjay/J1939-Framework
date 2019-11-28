662cd41bf2a579d5c0b973819fbd6a0724617895
Jay Tu <arden.jay@gmail.com>
2019-11-28 16:28:11 +0800

    Adds a test script for eton transmission
    Used as:
    ./eton.sh vcan0 | j1939Sender --silent --file tc1_frames.j1939
    
    Signed-off-by: Jay Tu <arden.jay@gmail.com>

---
+++ Scripts/eton.sh
@@ -0,0 +1,8 @@
+echo "set frame tc1 spn: 525 value: 249"
+echo "send frame tc1 interface: ${1}"
+sleep 5
+echo "set frame tc1 spn: 525 value: 247"
+sleep 5
+echo "set frame tc1 spn: 525 value: 249"
+sleep 5
+echo "set frame tc1 spn: 525 value: 247"

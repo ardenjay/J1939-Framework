d016216727633e30757737957defb2c28bd8f9c9
JayTu <jay_tu@fic.com.tw>
2019-12-20 17:33:18 +0800

    Create Event Handler
    When the packet callback is called, it can use different handler
    according to different command packet it received.
    
    Signed-off-by: JayTu <jay_tu@fic.com.tw>

--- web/backend/server.cpp
+++ web/backend/server.cpp
@@ -5,18 +5,46 @@ extern "C" {
 #include <string>
 #include <iostream>
 #include <queue>
+#include <functional>
 
 #include <json/json.h>
 
 #include <J1939Factory.h>
 #include <GenericFrame.h>
+#include <SPN/SPNNumeric.h>
+#include <SPN/SPNStatus.h>
+#include <SPN/SPNString.h>
 
-#define CMD_LIST "list frames"
+#define CMD_LIST		"list frames"
+#define CMD_REQ_FRAME	"req frame"
 
 using namespace std;
 using namespace J1939;
 
 static std::queue<Json::Value> respQueue;
+
+class EventHandler {
+private:
+	typedef std::function<bool(const Json::Value&, Json::Value&)> Handler;
+	std::map<string, Handler> m_handler;
+
+public:
+	int addListener(string event, Handler handler) {
+		m_handler[event] = handler;
+		return 0;
+	}
+
+	void removeListener(string event) {
+		m_handler.erase(event);
+	}
+
+	bool emitEvent(string event, const Json::Value& cmd, Json::Value& reply) {
+		Handler h = m_handler[event];
+		return h(cmd, reply);
+	}
+};
+
+static EventHandler eventHandler;
 
 static int callback_http(struct lws *wsi, enum lws_callback_reasons reason,
                          void *user, void *in, size_t len) {
@@ -33,25 +61,114 @@ static int callback_http(struct lws *wsi, enum lws_callback_reasons reason,
   return 0;
 }
 
-static bool process_cmd(const Json::Value& cmd, Json::Value& reply)
+static Json::Value
+spnToJson(const SPN *spn)
+{
+	Json::Value val;
+
+	val["name"] = spn->getName();
+	val["number"] = spn->getSpnNumber();
+	val["type"] = spn->getType();
+
+	switch(spn->getType()) {
+	case SPN::SPN_NUMERIC: {
+		const SPNNumeric *spnNum = static_cast<const SPNNumeric *>(spn);
+
+		val["value"] = spnNum->getFormattedValue();
+		val["units"] = spnNum->getUnits();
+		break;
+	}
+	case SPN::SPN_STATUS: {
+		const SPNStatus *spnStat = static_cast<const SPNStatus *>(spn);
+		val["value"] = spnStat->getValue();
+
+		SPNStatus::DescMap descriptions = spnStat->getValueDescriptionsMap();
+		for(auto desc = descriptions.begin(); desc != descriptions.end(); ++desc)
+			val["descriptions"][desc->first] = desc->second;
+		break;
+	}
+	case SPN::SPN_STRING: {
+		const SPNString *spnStr = static_cast<const SPNString *>(spn);
+		val["value"] = spnStr->getValue();
+		break;
+	}
+	defalut:
+		break;
+	}
+	return val;
+}
+
+static void frameToJson(string name, Json::Value& jsonVal)
+{
+	Json::Value jsonSpn;
+	std::unique_ptr<J1939Frame> frame;
+
+	frame = J1939Factory::getInstance().getJ1939Frame(name);
+	if (frame == nullptr)
+		return;
+
+	jsonVal["pgn"] = (u32)(frame->getPGN());
+	jsonVal["name"] = frame->getName();
+	jsonVal["priority"] = frame->getPriority();
+	jsonVal["source"] = frame->getSrcAddr();
+
+	if(frame->getPDUFormatGroup() == PDU_FORMAT_1)
+		jsonVal["dest"] = frame->getDstAddr();
+
+	if(frame->isGenericFrame()) {
+		const GenericFrame *genFrame = (const GenericFrame *) frame.get();
+		std::set<u32> spnNumbers = genFrame->getSPNNumbers();
+
+		for (auto s : spnNumbers) {
+			const SPN *spn = genFrame->getSPN(s);
+			Json::Value val = spnToJson(spn);
+			cout << "spn: " << val << endl;;
+
+			jsonSpn.append(val);
+		}
+	}
+	jsonVal["spns"] = jsonSpn;
+}
+
+static bool process_cmd_list(const Json::Value& cmd, Json::Value& reply)
 {
 	int index = 0;
+	set<u32> pgns = J1939Factory::getInstance().getAllRegisteredPGNs();
 
-	cout << "process_cmd: " << cmd["command"] << endl;;
-	if (cmd["command"] == CMD_LIST) {
-		reply["command"] = CMD_LIST;
-		set<u32> pgns = J1939Factory::getInstance().getAllRegisteredPGNs();
-		for (auto p : pgns) {
-			unique_ptr<J1939Frame> frame = J1939Factory::getInstance().getJ1939Frame(p);
-			if (frame->isGenericFrame()) {
-				reply["data"][index]["name"] = frame->getName();
-				reply["data"][index]["pgn"] = std::to_string(p);
-				index++;
-			}
+	reply["command"] = CMD_LIST;
+
+	for (auto p : pgns) {
+		unique_ptr<J1939Frame> frame = J1939Factory::getInstance().getJ1939Frame(p);
+		if (frame->isGenericFrame()) {
+			reply["data"][index]["name"] = frame->getName();
+			reply["data"][index]["pgn"] = std::to_string(p);
+			index++;
 		}
-		return true;
-	}	
-	return false;
+	}
+	return true;
+}
+
+static bool process_req_frame(const Json::Value& cmd, Json::Value& reply)
+{
+	u32 pgn;
+	string name = cmd["data"].asString();
+
+	if (name.empty()) {
+		cerr << __func__ << "data is null" << endl;
+		return false;
+	}
+
+	reply["command"] = CMD_REQ_FRAME;
+	frameToJson(name, reply);
+	return true;
+}
+
+static bool process_cmd(const Json::Value& cmd, Json::Value& reply)
+{
+	string event = cmd["command"].asString();
+
+	cout << "process_cmd: " << event << endl;
+	return eventHandler.emitEvent(event, cmd, reply);
 }
 
 static void send(struct lws *wsi, string s)
@@ -95,17 +212,17 @@ static int callback_j1939(struct lws *wsi, enum lws_callback_reasons reason,
 	case LWS_CALLBACK_RECEIVE: {
 		Json::CharReaderBuilder builder;
 		Json::CharReader *jSonReader = builder.newCharReader();
-		Json::Value value;
+		Json::Value cmd;
 		Json::Value reply;
 		string req, errs;
 
 		req.append((char *)in, len);
 
 		if (jSonReader->parse(req.c_str(), req.c_str() + req.size(),
-					&value, &errs)) {
-			if (value.isMember("command")) {
+					&cmd, &errs)) {
+			if (cmd.isMember("command")) {
 
-				if (process_cmd(value, reply))
+				if (process_cmd(cmd, reply))
 					respQueue.push(reply);
 
 				// request a callback
@@ -148,58 +265,66 @@ static struct lws_protocols protocols[] = {
     }
 };
 
+static void registerEvent()
+{
+	eventHandler.addListener(CMD_LIST, process_cmd_list);
+	eventHandler.addListener(CMD_REQ_FRAME, process_req_frame);
+}
+
 int main(void) {
-  struct lws_context *context;
-  struct lws_context_creation_info info;
-  struct lws_http_mount mount;
+	struct lws_context *context;
+	struct lws_context_creation_info info;
+	struct lws_http_mount mount;
 
-  memset(&mount, 0, sizeof mount);
-  mount.mountpoint = "/";
-  mount.origin = ".";
-  mount.def = "index.html";
-  mount.extra_mimetypes = NULL;
-  mount.origin_protocol = LWSMPRO_FILE;
-  mount.mountpoint_len = 1;
+	memset(&mount, 0, sizeof mount);
+	mount.mountpoint = "/";
+	mount.origin = ".";
+	mount.def = "index.html";
+	mount.extra_mimetypes = NULL;
+	mount.origin_protocol = LWSMPRO_FILE;
+	mount.mountpoint_len = 1;
 
-  memset(&info, 0, sizeof info);
-  info.port = 8000;
-  info.protocols = protocols;
-  info.mounts = &mount;
-  info.gid = -1;
-  info.uid = -1;
+	memset(&info, 0, sizeof info);
+	info.port = 8000;
+	info.protocols = protocols;
+	info.mounts = &mount;
+	info.gid = -1;
+	info.uid = -1;
 
-  lws_set_log_level(
-      LLL_ERR | LLL_WARN | LLL_NOTICE | LLL_INFO /*  | LLL_DEBUG */, NULL);
+	lws_set_log_level(
+			LLL_ERR | LLL_WARN | LLL_NOTICE | LLL_INFO /*  | LLL_DEBUG */, NULL);
 
-  context = lws_create_context(&info);
+	context = lws_create_context(&info);
 
-  if (context == NULL) {
-    fprintf(stderr, "libwebsocket init failed\n");
-    return -1;
-  }
+	if (context == NULL) {
+		fprintf(stderr, "libwebsocket init failed\n");
+		return -1;
+	}
 
-  lwsl_info("starting server...\n");
+	lwsl_info("starting server...\n");
 
-  J1939DataBase db;
-  if (!db.parseJsonFile(DATABASE_PATH)) {
-	  cerr << "Database not found in " << DATABASE_PATH << endl;
-	  return -EIO;
-  }
+	J1939DataBase db;
+	if (!db.parseJsonFile(DATABASE_PATH)) {
+		cerr << "Database not found in " << DATABASE_PATH << endl;
+		return -EIO;
+	}
 
-  const vector<GenericFrame>& dbFrames = db.getParsedFrames();
-  for (auto f : dbFrames)
-	  J1939Factory::getInstance().registerFrame(f);
+	const vector<GenericFrame>& dbFrames = db.getParsedFrames();
+	for (auto f : dbFrames)
+		J1939Factory::getInstance().registerFrame(f);
 
-  // infinite loop, the only option to end this serer is
-  // by sending SIGTERM. (CTRL+C)
-  while (1) {
-    lws_service(context, 50);
-    // libwebsocket_service will process all waiting events with their
-    // callback functions and then wait 50 ms.
-    // (this is single threaded webserver and this will keep
-    // our server from generating load while there are not
-    // requests to process)
-  }
+	registerEvent();
 
-  lws_context_destroy(context);
+	// infinite loop, the only option to end this serer is
+	// by sending SIGTERM. (CTRL+C)
+	while (1) {
+		lws_service(context, 50);
+		// libwebsocket_service will process all waiting events with their
+		// callback functions and then wait 50 ms.
+		// (this is single threaded webserver and this will keep
+		// our server from generating load while there are not
+		// requests to process)
+	}
+
+	lws_context_destroy(context);
 }

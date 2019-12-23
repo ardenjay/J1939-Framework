f99ac624852acf24e26c93b1834dde18e51117fe
JayTu <jay_tu@fic.com.tw>
2019-12-23 13:15:21 +0800

    use EventTarget for handling server event

--- web/frontend/src/app/frame/j1939-frame/j1939-frame.component.ts
+++ web/frontend/src/app/frame/j1939-frame/j1939-frame.component.ts
@@ -5,9 +5,16 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
   templateUrl: './j1939-frame.component.html',
   styleUrls: ['./j1939-frame.component.css']
 })
+
 export class J1939FrameComponent implements OnInit {
   private ws: WebSocket;
   private framelists = Array<{ name: string, pgn: string }>();
+  private target: EventTarget;
+
+  private frameChosen;
+
+  readonly CMD_LIST = "list frames";
+  readonly CMD_REQ = "req frame";
 
   public inputAddr = "";
 
@@ -16,6 +23,9 @@ export class J1939FrameComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.target = new EventTarget;
+    this.target.addEventListener(this.CMD_LIST, this.processListFrames);
+    this.target.addEventListener(this.CMD_REQ, this.processReqFrame);
   }
 
   add(_name: string, _pgn: string) {
@@ -23,13 +33,19 @@ export class J1939FrameComponent implements OnInit {
     this.framelists.push({ name: _name, pgn: _pgn });
   }
 
-  processCmd(cmd: string, data) {
-    if (cmd == "list frames") {
-      for (const d in data)
-        this.add(data[d].name, data[d].pgn);
-    } else {
-      console.log("unknown command: " + cmd);
-    }
+  processListFrames(event: CustomEvent) {
+    var detail = event.detail;
+    if (detail == null)
+      return;
+
+    var self = detail.self;
+    var data = detail.data;
+    for (const d in data)
+      self.add(data[d].name, data[d].pgn);
+  }
+
+  processReqFrame(event: CustomEvent) {
+
   }
 
   ConnectServer() {
@@ -45,22 +61,80 @@ export class J1939FrameComponent implements OnInit {
       this.ws = new WebSocket(host, "j1939-protocol");
 
     this.ws.onopen = function (evt) {
-      var cmd = { "command": "list frames" };
+      var cmd = { "command": self.CMD_LIST };
       var req = JSON.stringify(cmd);
 
       self.ws.send(req);
     };
 
-    this.ws.onmessage = function (evt) {
-      var jsonObj = JSON.parse(evt.data);
-      var cmd = jsonObj.command;
-      var data = jsonObj.data;
+    var printError = function (error, explicit) {
+      console.log(`[${explicit ? 'EXPLICIT' : 'INEXPLICIT'}] ${error.name}: ${error.message}`);
+    }
 
-      self.processCmd(cmd, data);
+    this.ws.onmessage = function (evt) {
+      var cmd;
+      var data;
+
+      try {
+        var jsonObj = JSON.parse(evt.data);
+      } catch (e) {
+        if (e instanceof SyntaxError) {
+          printError(e, true);
+          return;
+        } else {
+          printError(e, false);
+          return;
+        }
+      }
+
+      if (jsonObj == null) {
+        console.log("onmessage: jsonObj is null");
+        return;
+      }
+
+      if (jsonObj.hasOwnProperty("command"))
+        cmd = jsonObj.command;
+      else {
+        console.log("onmessage: missing command");
+        return;
+      }
+
+      if (jsonObj.hasOwnProperty("data"))
+        data = jsonObj.data;
+
+      console.log("onmessage: " + cmd);
+
+      // fire event to our handler
+      var event = new CustomEvent(cmd, {
+        detail: {
+          self,
+          data
+        }
+      });
+      self.target.dispatchEvent(event);
     };
 
     this.ws.onclose = function (evt) {
       console.log("Connection closed.");
     };
+  }
+
+  send(cmd) {
+    if (this.ws == null) {
+      console.log("No websocket connection");
+      return;
+    }
+    var req = JSON.stringify(cmd);
+    this.ws.send(req);
+  }
+
+  chooseFrame(value) {
+    console.log("chooseFrame: " + this.frameChosen);
+
+    var cmd = {
+      "command": this.CMD_REQ,
+      "data": this.frameChosen
+    };
+    this.send(cmd);
   }
 }

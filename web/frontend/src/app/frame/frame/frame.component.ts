4fba3982d2835268837e471cacddf3b2410e9b6a
JayTu <jay_tu@fic.com.tw>
2020-01-02 14:57:47 +0800

    Use tab instead of spaces

--- web/frontend/src/app/frame/frame/frame.component.ts
+++ web/frontend/src/app/frame/frame/frame.component.ts
@@ -1,106 +1,106 @@
 import { Component, OnInit, Input, ChangeDetectorRef, EventEmitter, Output } from '@angular/core';
 
 @Component({
-    selector: 'app-frame',
-    templateUrl: './frame.component.html',
-    styleUrls: ['./frame.component.css']
+	selector: 'app-frame',
+	templateUrl: './frame.component.html',
+	styleUrls: ['./frame.component.css']
 })
 export class FrameComponent implements OnInit {
-    @Input() public frameItem: FrameComponent;
-    @Output() frameEvent = new EventEmitter<{ spn: number, value: number, status: string }>();
+	@Input() public frameItem: FrameComponent;
+	@Output() frameEvent = new EventEmitter<{ spn: number, value: number, status: string }>();
 
-    public dest: number;
-    public name: string;
-    public pgn: string;
-    public prio: number;
-    public source: number;
-    public spns;
-    public interface: string;
-    public period: number;
-    public updated: boolean;
+	public dest: number;
+	public name: string;
+	public pgn: string;
+	public prio: number;
+	public source: number;
+	public spns;
+	public interface: string;
+	public period: number;
+	public updated: boolean;
 
-    private inputPriority;
-    private inputSource;
-    private inputDest;
-    private inputPeriod;
+	private inputPriority;
+	private inputSource;
+	private inputDest;
+	private inputPeriod;
 
-    private spnsArray = Array<{
-        spn: string,
-        value: string,
-        status: string
-    }>();
+	private spnsArray = Array<{
+		spn: string,
+		value: string,
+		status: string
+	}>();
 
-    constructor() { }
+	constructor() { }
 
-    ngOnChanges() {
-        this.showFrame();
-    }
+	ngOnChanges() {
+		this.showFrame();
+	}
 
-    ngOnInit() {
-    }
+	ngOnInit() {
+	}
 
-    notify(spn) {
-        this.frameEvent.emit(spn);
-    }
+	notify(spn) {
+		this.frameEvent.emit(spn);
+	}
 
-    isNumber(n): boolean {
-        return (typeof n != 'undefined') && (!Number.isNaN(n));
-    }
+	isNumber(n): boolean {
+		return (typeof n != 'undefined') && (!Number.isNaN(n));
+	}
 
-    showFrame() {
-        if (this.frameItem.updated == false)
-            return;
+	showFrame() {
+		if (this.frameItem.updated == false)
+			return;
 
-        this.name = this.frameItem.name;
-        this.pgn = this.frameItem.pgn;
-        this.interface = this.frameItem.interface;
+		this.name = this.frameItem.name;
+		this.pgn = this.frameItem.pgn;
+		this.interface = this.frameItem.interface;
 
-        if (this.isNumber(this.frameItem.prio))
-            this.prio = this.inputPriority = +(this.frameItem.prio);
-        if (this.isNumber(this.frameItem.source))
-            this.source = this.inputSource = +(this.frameItem.source);
-        if (this.isNumber(this.frameItem.dest))
-            this.dest = this.inputDest = +(this.frameItem.dest);
-    }
+		if (this.isNumber(this.frameItem.prio))
+			this.prio = this.inputPriority = +(this.frameItem.prio);
+		if (this.isNumber(this.frameItem.source))
+			this.source = this.inputSource = +(this.frameItem.source);
+		if (this.isNumber(this.frameItem.dest))
+			this.dest = this.inputDest = +(this.frameItem.dest);
+	}
 
-    AddSpn(s) {
-        this.spnsArray.push({ spn: "", value: "", status: "" });
-    }
+	AddSpn(s) {
+		this.spnsArray.push({ spn: "", value: "", status: "" });
+	}
 
-    DeleteSpn() {
-        this.spnsArray.pop();
-    }
+	DeleteSpn() {
+		this.spnsArray.pop();
+	}
 
-    changeSpn(spnObj, isSpn: boolean, value: number) {
-        if (isSpn)
-            spnObj.spn = value;
-        else
-            spnObj.value = value;
-    }
+	changeSpn(spnObj, isSpn: boolean, value: number) {
+		if (isSpn)
+			spnObj.spn = value;
+		else
+			spnObj.value = value;
+	}
 
-    Send(spn) {
-        if (spn.spn.length == 0 || spn.value.length == 0) {
-            alert("Spn or Value is empty");
-            return;
-        }
+	Send(spn) {
+		if (spn.spn.length == 0 || spn.value.length == 0) {
+			alert("Spn or Value is empty");
+			return;
+		}
 
-        // FIXME
-        spn.status = "Success";
+		// FIXME
+		spn.status = "Success";
 
-        // refetch the user input value
-        if (this.isNumber(this.inputPriority))
-            this.prio = +(this.inputPriority);
+		// refetch the user input value
+		if (this.isNumber(this.inputPriority))
+			this.prio = +(this.inputPriority);
 
-        if (this.isNumber(this.inputSource))
-            this.source = +(this.inputSource);
+		if (this.isNumber(this.inputSource))
+			this.source = +(this.inputSource);
 
-        if (this.isNumber(this.inputDest))
-            this.dest = +(this.inputDest);
+		if (this.isNumber(this.inputDest))
+			this.dest = +(this.inputDest);
 
-        this.period = this.inputPeriod;
-        if (this.period == undefined)
-          this.period = 0;
-        
-        this.notify(spn);
-    }
+		this.period = this.inputPeriod;
+		if (this.period == undefined)
+			this.period = 0;
+
+		this.notify(spn);
+	}
 }
--- web/frontend/src/app/frame/j1939-frame/j1939-frame.component.ts
+++ web/frontend/src/app/frame/j1939-frame/j1939-frame.component.ts
@@ -2,280 +2,280 @@ import { FrameComponent } from './../frame/frame.component';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'app-j1939-frame',
-  templateUrl: './j1939-frame.component.html',
-  styleUrls: ['./j1939-frame.component.css']
+	selector: 'app-j1939-frame',
+	templateUrl: './j1939-frame.component.html',
+	styleUrls: ['./j1939-frame.component.css']
 })
 
 export class J1939FrameComponent implements OnInit {
-  // components
-  private ws: WebSocket;
-  private target: EventTarget;
+	// components
+	private ws: WebSocket;
+	private target: EventTarget;
 
-  /* cmd list */
-  /* get all supported frames */
-  readonly CMD_LIST = "list frames";
-  /* get frame info according to selected frame */
-  readonly CMD_REQ = "req frame";
-  /* get baud rate */
-  readonly CMD_BAUD = "set baud rate"
-  readonly CMD_CREATE_FRAME = "create frame"
+	/* cmd list */
+	/* get all supported frames */
+	readonly CMD_LIST = "list frames";
+	/* get frame info according to selected frame */
+	readonly CMD_REQ = "req frame";
+	/* get baud rate */
+	readonly CMD_BAUD = "set baud rate"
+	readonly CMD_CREATE_FRAME = "create frame"
 
-  // class private member
-  private framelists = Array<{ name: string, pgn: string }>();
-  private frameChosen;
-  private inputAddr = "";
-  private inputBaud = 250;
+	// class private member
+	private framelists = Array<{ name: string, pgn: string }>();
+	private frameChosen;
+	private inputAddr = "";
+	private inputBaud = 250;
 
-  private canIf = Array<{
-    name: string,
-    value: number,
-  }>();
+	private canIf = Array<{
+		name: string,
+		value: number,
+	}>();
 
-  // control whether to show frame
-  private showFrame: boolean;
-  private interface: string;
-  @ViewChild(FrameComponent, { static: false })
-  private frameComponent: FrameComponent;
+	// control whether to show frame
+	private showFrame: boolean;
+	private interface: string;
+	@ViewChild(FrameComponent, { static: false })
+	private frameComponent: FrameComponent;
 
-  constructor() {
-    this.ws = null;
-  }
+	constructor() {
+		this.ws = null;
+	}
 
-  ngOnInit() {
-    this.target = new EventTarget;
-    this.target.addEventListener(this.CMD_LIST, this.processListFrames);
-    this.target.addEventListener(this.CMD_REQ, this.processReqFrame);
-    this.target.addEventListener(this.CMD_BAUD, this.processSetBaud);
-    // test
-    /* this.add("jay", "111"); */
+	ngOnInit() {
+		this.target = new EventTarget;
+		this.target.addEventListener(this.CMD_LIST, this.processListFrames);
+		this.target.addEventListener(this.CMD_REQ, this.processReqFrame);
+		this.target.addEventListener(this.CMD_BAUD, this.processSetBaud);
+		// test
+		/* this.add("jay", "111"); */
 
-    /* this.canIf.push({ name: "vcan0", value: 0});
-    this.canIf.push({ name: "CAN0", value: 1}); */
-  }
+		/* this.canIf.push({ name: "vcan0", value: 0});
+		this.canIf.push({ name: "CAN0", value: 1}); */
+	}
 
-  add(_name: string, _pgn: string) {
-    // console.log("add: " + _name + ", pgn: " + _pgn);
-    this.framelists.push({ name: _name, pgn: _pgn });
-  }
+	add(_name: string, _pgn: string) {
+		// console.log("add: " + _name + ", pgn: " + _pgn);
+		this.framelists.push({ name: _name, pgn: _pgn });
+	}
 
-  processListFrames(event: CustomEvent) {
-    var detail = event.detail;
-    if (detail == null)
-      return;
+	processListFrames(event: CustomEvent) {
+		var detail = event.detail;
+		if (detail == null)
+			return;
 
-    var self = detail.self;
-    var data = detail.data;
-    for (const d in data)
-      self.add(data[d].name, data[d].pgn);
-  }
+		var self = detail.self;
+		var data = detail.data;
+		for (const d in data)
+			self.add(data[d].name, data[d].pgn);
+	}
 
-  processReqFrame(event: CustomEvent) {
-    var detail = event.detail;
-    if (detail == null)
-      return;
+	processReqFrame(event: CustomEvent) {
+		var detail = event.detail;
+		if (detail == null)
+			return;
 
-    var self = detail.self;
-    var data = detail.data;
+		var self = detail.self;
+		var data = detail.data;
 
-    self.frameComponent = new FrameComponent();
-    self.frameComponent.dest = data["dest"];
-    self.frameComponent.name = data["name"];
-    self.frameComponent.pgn = data["pgn"];
-    self.frameComponent.prio = data["priority"];
-    self.frameComponent.source = data["source"];
-    self.frameComponent.updated = true;
-    if (self.interface == null)
-      self.frameComponent.interface = "vcan0";  // default VCAN0
-    else
-      self.frameComponent.interface = self.interface;
-    self.showFrame = true;
-  }
+		self.frameComponent = new FrameComponent();
+		self.frameComponent.dest = data["dest"];
+		self.frameComponent.name = data["name"];
+		self.frameComponent.pgn = data["pgn"];
+		self.frameComponent.prio = data["priority"];
+		self.frameComponent.source = data["source"];
+		self.frameComponent.updated = true;
+		if (self.interface == null)
+			self.frameComponent.interface = "vcan0";  // default VCAN0
+		else
+			self.frameComponent.interface = self.interface;
+		self.showFrame = true;
+	}
 
-  processSetBaud(event: CustomEvent) {
-    var detail = event.detail;
-    if (detail == null)
-      return;
+	processSetBaud(event: CustomEvent) {
+		var detail = event.detail;
+		if (detail == null)
+			return;
 
-    console.log("processSetBaud");
-    var self = detail.self;
-    var data = detail.data;
+		console.log("processSetBaud");
+		var self = detail.self;
+		var data = detail.data;
 
-    for (const d in data) {
-      var radio = {
-        name: data[d],
-        value: d
-      }
-      self.canIf.push(radio);
-    }
+		for (const d in data) {
+			var radio = {
+				name: data[d],
+				value: d
+			}
+			self.canIf.push(radio);
+		}
 
-    if (data == null)
-      console.log("there is no interface");
-  }
+		if (data == null)
+			console.log("there is no interface");
+	}
 
-  ConnectServer() {
-    var self = this;
+	ConnectServer() {
+		var self = this;
 
-    if (this.inputAddr.length == 0)
-      this.inputAddr = "127.0.0.1";
+		if (this.inputAddr.length == 0)
+			this.inputAddr = "127.0.0.1";
 
-    if (this.inputBaud == 0)
-      this.inputBaud = 250; // 250k
+		if (this.inputBaud == 0)
+			this.inputBaud = 250; // 250k
 
-    let host = "ws://" + this.inputAddr + ":8000"
-    console.log("Connection to " + host);
+		let host = "ws://" + this.inputAddr + ":8000"
+		console.log("Connection to " + host);
 
-    if (this.ws == null)
-      this.ws = new WebSocket(host, "j1939-protocol");
+		if (this.ws == null)
+			this.ws = new WebSocket(host, "j1939-protocol");
 
-    this.ws.onopen = function (evt) {
-      var cmd = {
-        "command": self.CMD_BAUD,
-        "data": self.inputBaud
-      };
-      self.send(cmd);
-    };
+		this.ws.onopen = function (evt) {
+			var cmd = {
+				"command": self.CMD_BAUD,
+				"data": self.inputBaud
+			};
+			self.send(cmd);
+		};
 
-    var printError = function (error, explicit) {
-      console.log(`[${explicit ? 'EXPLICIT' : 'INEXPLICIT'}] ${error.name}: ${error.message}`);
-    }
+		var printError = function (error, explicit) {
+			console.log(`[${explicit ? 'EXPLICIT' : 'INEXPLICIT'}] ${error.name}: ${error.message}`);
+		}
 
-    this.ws.onmessage = function (evt) {
-      var cmd;
-      var data;
+		this.ws.onmessage = function (evt) {
+			var cmd;
+			var data;
 
-      try {
-        var jsonObj = JSON.parse(evt.data);
-      } catch (e) {
-        if (e instanceof SyntaxError) {
-          printError(e, true);
-          return;
-        } else {
-          printError(e, false);
-          return;
-        }
-      }
+			try {
+				var jsonObj = JSON.parse(evt.data);
+			} catch (e) {
+				if (e instanceof SyntaxError) {
+					printError(e, true);
+					return;
+				} else {
+					printError(e, false);
+					return;
+				}
+			}
 
-      if (jsonObj == null) {
-        console.log("onmessage: jsonObj is null");
-        return;
-      }
+			if (jsonObj == null) {
+				console.log("onmessage: jsonObj is null");
+				return;
+			}
 
-      if (jsonObj.hasOwnProperty("command"))
-        cmd = jsonObj.command;
-      else {
-        console.log("onmessage: missing command");
-        return;
-      }
+			if (jsonObj.hasOwnProperty("command"))
+				cmd = jsonObj.command;
+			else {
+				console.log("onmessage: missing command");
+				return;
+			}
 
-      if (jsonObj.hasOwnProperty("data"))
-        data = jsonObj.data;
+			if (jsonObj.hasOwnProperty("data"))
+				data = jsonObj.data;
 
-      console.log("onmessage: " + cmd);
+			console.log("onmessage: " + cmd);
 
-      // fire event to our handler
-      var event = new CustomEvent(cmd, {
-        detail: {
-          self,
-          data
-        }
-      });
-      self.target.dispatchEvent(event);
-    };
+			// fire event to our handler
+			var event = new CustomEvent(cmd, {
+				detail: {
+					self,
+					data
+				}
+			});
+			self.target.dispatchEvent(event);
+		};
 
-    this.ws.onerror = function (evt) {
-      console.log("error");
-      self.ws = null;
-      alert("Server connection fails");
-    }
+		this.ws.onerror = function (evt) {
+			console.log("error");
+			self.ws = null;
+			alert("Server connection fails");
+		}
 
-    this.ws.onclose = function (evt) {
-      console.log("Connection closed");
-      self.ws = null;
-    };
-  }
+		this.ws.onclose = function (evt) {
+			console.log("Connection closed");
+			self.ws = null;
+		};
+	}
 
-  send(cmd) {
-    // open: 1
-    if ((this.ws == null) || (this.ws.readyState != 1)) {
-      console.log("No websocket connection");
-      alert("No websocket connection");
-      return;
-    }
-    var req = JSON.stringify(cmd);
-    this.ws.send(req);
-  }
+	send(cmd) {
+		// open: 1
+		if ((this.ws == null) || (this.ws.readyState != 1)) {
+			console.log("No websocket connection");
+			alert("No websocket connection");
+			return;
+		}
+		var req = JSON.stringify(cmd);
+		this.ws.send(req);
+	}
 
-  chooseFrame(value) {
-    console.log("chooseFrame: " + this.frameChosen);
+	chooseFrame(value) {
+		console.log("chooseFrame: " + this.frameChosen);
 
-    var cmd = {
-      "command": this.CMD_REQ,
-      "data": this.frameChosen
-    };
-    this.send(cmd);
+		var cmd = {
+			"command": this.CMD_REQ,
+			"data": this.frameChosen
+		};
+		this.send(cmd);
 
-    /* var data = {
-      "dest": 254,
-      "name": "TC1",
-      "pgn": 256,
-      "priority": 0,
-      "source": 254,
-      "spns":
-        [
-          {
-            "name": "Transmission Requested Gear",
-            "number": 525,
-            "type": 0,
-            "units": "gear value",
-            "value": 4294967295.0
-          }
-        ]
-    };
+		/* var data = {
+		  "dest": 254,
+		  "name": "TC1",
+		  "pgn": 256,
+		  "priority": 0,
+		  "source": 254,
+		  "spns":
+			[
+			  {
+				"name": "Transmission Requested Gear",
+				"number": 525,
+				"type": 0,
+				"units": "gear value",
+				"value": 4294967295.0
+			  }
+			]
+		};
+	
+		var self = this;
+		var event = new CustomEvent(this.CMD_REQ, {
+		  detail: {
+			self,
+			data
+		  }
+		});
+		this.target.dispatchEvent(event);
+		*/
+	}
 
-    var self = this;
-    var event = new CustomEvent(this.CMD_REQ, {
-      detail: {
-        self,
-        data
-      }
-    });
-    this.target.dispatchEvent(event);
-    */
-  }
+	ClearInput() {
+		this.frameChosen = "";
+	}
 
-  ClearInput() {
-    this.frameChosen = "";
-  }
+	GetFrames() {
+		var cmd = { "command": this.CMD_LIST };
+		this.send(cmd);
+	}
 
-  GetFrames() {
-    var cmd = { "command": this.CMD_LIST };
-    this.send(cmd);
-  }
+	changeInterface(value) {
+		console.log("changeInterface: " + value);
+		this.interface = value;
+	}
 
-  changeInterface(value) {
-    console.log("changeInterface: " + value);
-    this.interface = value;
-  }
+	receiveEvent(spn) {
+		console.log("receiveEvent: spn: " + spn.spn + " value: " + spn.value);
 
-  receiveEvent(spn) {
-    console.log("receiveEvent: spn: " + spn.spn + " value: " + spn.value);
+		var cmd = {
+			"command": this.CMD_CREATE_FRAME,
+			"data": {
+				"dest": this.frameComponent.dest,
+				"name": this.frameComponent.name,
+				"pgn": this.frameComponent.pgn,
+				"priority": this.frameComponent.prio,
+				"source": this.frameComponent.source,
+				"interface": this.frameComponent.interface,
+				"period": this.frameComponent.period,
+				"spn": +(spn.spn),
+				"value": +(spn.value)
+			}
+		};
 
-    var cmd = {
-      "command": this.CMD_CREATE_FRAME,
-      "data": {
-        "dest": this.frameComponent.dest,
-        "name": this.frameComponent.name,
-        "pgn": this.frameComponent.pgn,
-        "priority": this.frameComponent.prio,
-        "source": this.frameComponent.source,
-        "interface": this.frameComponent.interface,
-        "period": this.frameComponent.period,
-        "spn": +(spn.spn),
-        "value": +(spn.value)
-      }
-    };
-
-    this.send(cmd);
-  }
+		this.send(cmd);
+	}
 }
